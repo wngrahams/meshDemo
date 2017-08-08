8bbd38f1835ac16b9c18a465072eb254a880d286
Graham Stubbs <grahamstubbs@Grahams-MacBook-Pro.local>
2017-08-09 00:22:44 +0200

    Initial Commit

---
+++ meshReduction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  meshReduction
+//
+//  Created by Graham Stubbs on 8/9/17.
+//  Copyright (c) 2017 Graham Stubbs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
