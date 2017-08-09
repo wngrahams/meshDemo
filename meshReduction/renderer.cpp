22493c20e24a9e67a0d10ec13dee536a0d232cfc
Graham Stubbs <23465768+wngrahams@users.noreply.github.com>
2017-08-09 22:40:39 +0200

    Draw basic triangle

--- meshReduction/renderer.cpp
+++ meshReduction/renderer.cpp
@@ -114,6 +114,35 @@ int main () {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     
+    //create vertex data for drawing
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f, //bottom left
+        0.5f, -0.5f, 0.0f, //bottom right
+        0.0f, 0.5f, 0.0f //top
+    };
+    
+    //create vertex buffer object (VBO) and vertex array object (VAO)
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    //bind VAO
+    glBindVertexArray(VAO);
+    
+    //bind and set VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    //create vertex pointer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *) 0);
+    
+    //enable vertex array
+    glEnableVertexAttribArray(0);
+    
+    //unbind buffer and array
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
     while (!glfwWindowShouldClose(window)) {
         //check if any events were acitivated
         glfwPollEvents();
@@ -123,10 +152,22 @@ int main () {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        //draw object
+        //indicate which shader program to use
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        
         //swap sceen buffers
         glfwSwapBuffers(window);
     }
     
+    //Deallocate resources
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    
+    //Terminate GLFW and clear any resources allocated by GLFW
     glfwTerminate();
     
     return 0;
