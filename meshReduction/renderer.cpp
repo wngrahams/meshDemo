3a37c8b3293c5363a42ee11405ccf98ec44ff923
Graham Stubbs <23465768+wngrahams@users.noreply.github.com>
2017-08-09 22:18:07 +0200

    Initialized basic shaders

--- meshReduction/renderer.cpp
+++ meshReduction/renderer.cpp
@@ -17,9 +17,26 @@
 
 const GLint WIDTH = 800, HEIGHT = 600; //screen dimensions
 
+const GLchar *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}";
+
+const GLchar *fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}";
+
 int main () {
     
+    //Initialize GLFW
     glfwInit();
+    
+    //set all required options for GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //openGL version 3.3
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -30,32 +47,83 @@ int main () {
     
     int screenWidth, screenHeight; //required for mac due to retina display
     glfwGetFramebufferSize(window, &screenWidth, &screenHeight); //required for mac due to retina display
-    std::cout << "screenWidth: " << screenWidth << ", screenHeight: " << screenHeight << std::endl;
+//    std::cout << "screenWidth: " << screenWidth << ", screenHeight: " << screenHeight << std::endl;
     
     if (nullptr == window) {
-        std::cout << "Failed to creat GLFW window!/n";
+        std::cout << "Failed to creat GLFW window!\n";
         glfwTerminate();
         
-        return -1;
+        return EXIT_FAILURE;
     }
     
     glfwMakeContextCurrent(window);
     
     glewExperimental = GL_TRUE; //fixes glew bug
     
+    //Initialize GLEW
     if (GLEW_OK != glewInit()) {
-        std::cout << "Failed to initialize GLEW!/n";
+        std::cout << "Failed to initialize GLEW!\n";
         
-        return -1;
+        return EXIT_FAILURE;
     }
     
+    //define viewport dimensions
     glViewport(0, 0, screenWidth, screenHeight);
     
+    //initialize and compile vertex shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    //check for vertex shader compilation errors
+    GLint success;
+    GLchar infoLog[512];
+    
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    //initialize and compile fragment shaders
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    //check for fragment shader compilation errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    //link shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    //check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    
+    //clean up shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
     while (!glfwWindowShouldClose(window)) {
+        //check if any events were acitivated
         glfwPollEvents();
+        
+        //render
+        //clear the colorbuffer
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        //swap sceen buffers
         glfwSwapBuffers(window);
     }
     
