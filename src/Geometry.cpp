b4c46f3044308ecfcc7510233658a706f0ef238e
Graham Stubbs <23465768+wngrahams@users.noreply.github.com>
2017-08-26 17:49:34 -0400

    Added Geometry::calculateVertexNormals

--- src/Geometry.cpp
+++ src/Geometry.cpp
@@ -10,6 +10,8 @@
 #include "Geometry.h"
 #include "VertexJoiner.h"
 
+//TODO: delete any "new" arrays in the destructor
+
 Geometry::Geometry (float *normals, float *vertices, int *indices, int numTris) {
     
     this->numTriangles = numTris;
@@ -18,6 +20,8 @@ Geometry::Geometry (float *normals, float *vertices, int *indices, int numTris) 
     this->faceNormals = normals;
     this->vertices = vertices;
     this->indices = indices;
+    
+    this->vertexNormals = nullptr;
     
     //join vertices, then calculate normals
     joinVertices();
@@ -94,9 +98,45 @@ void Geometry::calculateFaceNormals() {
 }
 
 void Geometry::calculateVertexNormals () {
+    if (nullptr == this->faceNormals)
+        calculateFaceNormals();
     
+    this->vertexNormals = new float[this->numVertices * 3];
     
+    for (int i=0; i < this->numVertices * 3; i++)
+        vertexNormals[i] = 0;
     
+    for (int i=0; i < this->numTriangles; i++) {
+        int i1 = indices[3 * i + 0];
+        int i2 = indices[3 * i + 1];
+        int i3 = indices[3 * i + 2];
+        
+        vertexNormals[3 * i1 + 0] += faceNormals[3 * i + 0];
+        vertexNormals[3 * i1 + 1] += faceNormals[3 * i + 1];
+        vertexNormals[3 * i1 + 2] += faceNormals[3 * i + 2];
+
+        vertexNormals[3 * i2 + 0] += faceNormals[3 * i + 0];
+        vertexNormals[3 * i2 + 1] += faceNormals[3 * i + 1];
+        vertexNormals[3 * i2 + 2] += faceNormals[3 * i + 2];
+        
+        vertexNormals[3 * i3 + 0] += faceNormals[3 * i + 0];
+        vertexNormals[3 * i3 + 1] += faceNormals[3 * i + 1];
+        vertexNormals[3 * i3 + 2] += faceNormals[3 * i + 2];
+    }
+    
+    glm::vec3 normal;
+    
+    for (int i=0; i < this->numVertices; i++) {
+        normal = {this->vertexNormals[3 * i + 0],
+                  this->vertexNormals[3 * i + 1],
+                  this->vertexNormals[3 * i + 2]};
+        
+        normal = glm::normalize(normal);
+        
+        this->vertexNormals[3 * i + 0] = normal.x;
+        this->vertexNormals[3 * i + 1] = normal.y;
+        this->vertexNormals[3 * i + 2] = normal.z;
+    }
 }
 
 
